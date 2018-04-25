449fd49db1d5caede6d166653258e7bef15690da
Phil Wareham <phil@designhive.com>
2018-04-25 12:11:25 +0100

    Retooling

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
+import {MDCCheckbox} from '@material/checkbox';
+import {MDCChip} from '@material/chips';
+import {MDCChipSet} from '@material/chips';
+import {MDCDialog} from '@material/dialog';
+import {MDCPersistentDrawer} from '@material/drawer';
+import {MDCTemporaryDrawer} from '@material/drawer';
+import {MDCFloatingLabel} from '@material/floating-label';
+import {MDCFormField} from '@material/form-field';
 import {MDCRipple} from '@material/ripple';
-const ripple = new MDCRipple(document.querySelector('.foo-button'));
+import {MDCGridList} from '@material/grid-list';
+import {MDCIconToggle} from '@material/icon-toggle';
+import {MDCLineRipple} from '@material/line-ripple';
+import {MDCLinearProgress} from '@material/linear-progress';
+import {MDCNotchedOutline} from '@material/notched-outline';
+import {MDCRadio} from '@material/radio';
+import {MDCSnackbar} from '@material/snackbar';
+import {MDCTab} from '@material/tabs';
+import {MDCTabBar} from '@material/tabs';
+import {MDCTextField} from '@material/textfield';
+import {MDCMenu} from '@material/menu';
+import {MDCSelect} from '@material/select';
+import {MDCSlider} from '@material/slider';
+import {MDCToolbar} from '@material/toolbar';
+import {MDCTopAppBar} from '@material/top-app-bar';
+
+
+const ripple = new MDCRipple(document.querySelector('.mdc-button'));
